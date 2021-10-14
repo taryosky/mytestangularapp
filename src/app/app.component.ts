9ac8ebea20c8e0898b8baf1bc3ecc3b3d15182f6
Azibataram Clement <clementazibataram@gmail.com>
2021-10-14 11:24:46 +0100

    ple commint
    Signed-off-by: Azibataram Clement <clementazibataram@gmail.com>

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
+  selector: 'app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  myName = "Clement";
+  myParam?: string;
   title = 'new-app';
+
+  handleEvent(para:string){
+      this.myParam = para;
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NewComponentComponent } from './new-component/new-component.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NewComponentComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
